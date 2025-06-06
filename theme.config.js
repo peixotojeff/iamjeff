cc86faed4732f3927384d39972340a3722e9e5fd
peixotojeff <jefferson.-peixoto@hotmail.com>
2025-06-06 11:05:12 -0300

    Update personal information and enhance footer content in theme configuration

--- theme.config.js
+++ theme.config.js
@@ -3,7 +3,7 @@ const YEAR = new Date().getFullYear()
 export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
-      <time>{YEAR}</time> © Your Name.
+      <time>{YEAR}</time> © Jefferson Peixoto.. Feito com <span style={{ color: 'red' }}>❤️</span> e muito café.
       <a href="/feed.xml">RSS</a>
       <style jsx>{`
         a {
